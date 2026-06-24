3e28f012a519069266c35817ce3b4e6c02e99fde
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-24 16:12:58 -0600

    Valida los datos

--- form.php
+++ form.php
@@ -1,7 +1,20 @@
 <?php
-// Sanitiza e imprime los datos
-echo htmlspecialchars($_POST['nombre'] ?? '', ENT_QUOTES, 'UTF-8');
-echo (int)htmlspecialchars($_POST['edad'] ?? 1, ENT_QUOTES, 'UTF-8');
+// Sanitiza los datos
+$nombre = htmlspecialchars($_POST['nombre'] ?? '', ENT_QUOTES, 'UTF-8');
+$edad = (int)htmlspecialchars($_POST['edad'] ?? 0, ENT_QUOTES, 'UTF-8');
+
+// Valida los datos
+if ($nombre === ""){
+	echo "ERROR: El nombre no debe estar vacio\n";
+}else{
+	echo sprintf("Nombre: %s\n", $nombre);
+}
+
+if ($edad < 1 || $edad > 120){
+	echo "ERROR: Edad no valida, por favor verifique\n";
+}else{
+	echo sprintf("Edad: %d\n", $edad);
+}
 
 ?>
 
