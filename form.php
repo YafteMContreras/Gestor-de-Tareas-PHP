b818e69763442adf0bc9a9331265f80542f641c5
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-24 15:53:56 -0600

    Crea formulario

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+?>
+
+<form method="POST" action="">
+	<label for="nombre">Ingresa tu nombre: </label>
+	<input type="TEXT" name="nombre">
+
+	<label for="edad">Ingresa tu edad: </label>
+	<input type="INT" name="edad">
+
+	<button type="submit">Guardar</button>
+</form>
