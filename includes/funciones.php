3928f4298adfb3247775161dc013ceca1fad35d3
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-29 22:21:48 -0600

    Coloca la lógica en el index

--- includes/funciones.php
+++ includes/funciones.php
@@ -11,24 +11,7 @@ function crearTarea(string $titulo, callable $valida, int $prioridad = 1, bool $
         return $tarea;
 }
 
-$tareas = [crearTarea("Estudiar PHP", validarTitulo(...), 2), crearTarea("Terminar proyecto", validarTitulo(...)), crearTarea("Leer documentación", validarTitulo(...), 3, true)];
-$tareas = array_filter($tareas);
 
-// Implementa lógica de registro de tareas
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        // Sanitiza los datos
-        $tarea_titulo = htmlspecialchars($_POST['titulo'] ?? '', ENT_QUOTES, 'UTF-8');
-        $tarea_prioridad = (int)$_POST['prioridad'];
-
-        if ($tarea_prioridad < 1 || $tarea_prioridad > 3){
-                echo "ERROR: Debe seleccionar un valor para la prioridad\n";
-        }else{
-                $nuevaTarea = crearTarea($tarea_titulo, validarTitulo(...), $tarea_prioridad);
-                if ($nuevaTarea !== []){
-                        $tareas[count($tareas)] = $nuevaTarea;
-                }
-        }
-}
 
 // Función que valida titulos
 function validarTitulo(string $titulo) : bool{
@@ -65,17 +48,4 @@ function imprimeTareas(array $array, string $texto, callable $textoPrioridad) : 
 <?php
 }
 
-// Ordena el arreglo por prioridad ascendente
-usort($tareas, fn($a, $b) => $a["prioridad"] <=> $b["prioridad"]);
-
-// Separa la lista en completadas y pendientes
-$completadas = array_filter($tareas, fn($a) => $a["completada"] === true);
-$completadas = array_values($completadas);
-
-$pendientes = array_filter($tareas, fn($a) => $a["completada"] === false);
-$pendientes = array_values($pendientes);
-
-// Imprime las listas
-imprimeTareas($completadas, "Tareas completadas", textoPrioridad(...));
-imprimeTareas($pendientes, "Tareas pendientes", textoPrioridad(...));
 ?>
