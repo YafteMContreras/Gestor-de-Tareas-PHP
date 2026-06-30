33edadc14c406eaffef9f44cbc3a6abcfab2ee70
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-29 21:58:53 -0600

    Separa el header del index

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<head>
-	<h1>Gestor de Tareas</h1>
-</head>
-<body>
-	<form method="POST" action="">
-		<h2>Registrar tarea</h2>
-		<label for "titulo">Ingresa el titulo:</label>
-		<input type="text" name="titulo">
-		<label for "prioridad">Establece la prioridad</label>
-		<select name="prioridad" id="prioridad_select">
-			<option value="">--Por favor seleccione una opción--</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-		</select>
-		<button type="submit">Registrar</button>
-	</form>
-</body>
-
 <?php
+require_once __DIR__ . '/includes/header.php';
 // Función que crea tareas
 function crearTarea(string $titulo, callable $valida, int $prioridad = 1, bool $completada = false) : array{
 	static $id = 0;
