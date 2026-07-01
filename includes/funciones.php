fa4be8131724ebcd2e4c1e6ff134f57c75569ece
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-06-30 23:32:33 -0600

    Borra funciones obsoletas

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,32 +1,4 @@
 <?php
-// Función que crea tareas
-function crearTarea(string $titulo, callable $valida, int $prioridad = 1, bool $completada = false) : array{
-        static $id = 0;
-        if (!$valida($titulo)){
-                echo "El titulo no debe estar vacío o tener menos de 3 caracteres\n";
-                return [];
-        }
-        $tarea = ["id" => $id, "titulo" => $titulo, "prioridad" => $prioridad, "completada" => $completada];
-        $id++;
-        return $tarea;
-}
-
-
-
-// Función que valida titulos
-function validarTitulo(string $titulo) : bool{
-        return preg_match('/^[\w]{3,}/',$titulo);
-}
-
-// Función para texto de prioridad
-function textoPrioridad(int $prioridad) : string{
-        return match($prioridad){
-                1 => "Alta",
-                2 => "Media",
-                3 => "Baja",
-        };
-}
-
 // Función que imprime tareas
 function imprimeTareas(array $array, string $texto) : void {
 ?>
