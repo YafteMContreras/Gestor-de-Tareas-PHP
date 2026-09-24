a10827ec92d2b9f18b6c23831d078029489adab0
YafteMContreras <montielcontrerasyafte@gmail.com>
2026-09-23 19:16:41 -0600

    Elimina el requerimiento de Validable.php ya que ese archivo fue eliminado

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/includes/TareaInvalidaException.php';
-require_once __DIR__ . '/includes/Validable.php';
 require_once __DIR__ . '/includes/ContadorTareas.php';
 require_once __DIR__ . '/includes/Tarea.php';
 require_once __DIR__ . '/includes/funciones.php';
